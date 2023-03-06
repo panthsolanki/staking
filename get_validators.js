b08a9158633c54cec41badbf21754177b63f7d79
Panth <panth.solanki@gmail.com>
2023-03-06 17:40:39 +0530

    validators added

---
+++ get_validators.js
@@ -0,0 +1,21 @@
+const { Connection, clusterApiUrl } = require("@solana/web3.js");
+
+
+const main = async () => {
+  const connection = new Connection(clusterApiUrl('devnet'), 'processed');
+  const { current, delimiters } = await connection.getVoteAccounts()
+
+  // console.log('all validators: ' + current.concat(delimiters).length)
+  // console.log('current validators: ' + current.length);
+  // console.log(current[0]);
+}
+
+const runMain = async() => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+runMain();
